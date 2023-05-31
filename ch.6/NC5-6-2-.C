f613e211c07b38e551bb121fd02f3b5b8353f8df
Nenavavdiya07 <nenavavdiya07@gmail.com>
2023-05-31 18:48:38 +0530

    ch.6

---
+++ ch.6/NC5-6-2-.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,n;
+	clrscr();
+		printf("Enter n: ");
+		scanf("%d",&n);
+
+	do
+	{
+		printf("%d ",i);
+		i++;
+
+	}while(i<=n);
+	getch();
+
+}
---
+++ ch.6/NC5-6-3.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i=10,n;
+	clrscr();
+
+	do
+	{
+		printf("%d ",i);
+		i--;
+
+	}while(i>=1);
+	getch();
+
+}
